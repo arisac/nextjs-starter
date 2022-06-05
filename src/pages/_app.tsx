fcd40b7b124123100c132ff5d372a566dad8d768
arisac <hi@aris.ac>
2022-06-05 01:22:10 -0700

    add App Component key with router.asPath

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import 'styles/style.scss'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
-      <Component {...pageProps} />
+      <Component key={router.asPath} {...pageProps} />
     </ThemeProvider>
   )
 }
